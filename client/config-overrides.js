b78ae354b52b243f8223e7a308340b0bb91da516
antoinech <42295406+antoinech2@users.noreply.github.com>
2024-01-15 21:23:28 +0100

    Added liste icon support

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -1,7 +1,33 @@
 const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
 
 module.exports = function override(config, env) {
-    config.resolve.plugins = config.resolve.plugins.filter(plugin => !(plugin instanceof ModuleScopePlugin));
+    var webpack = require('webpack');
 
+    config.resolve.plugins = config.resolve.plugins.filter(plugin => !(plugin instanceof ModuleScopePlugin));
+    const fallback = config.resolve.fallback || {}; 
+    Object.assign(fallback, { 
+        "child_process" : false,
+        "fs": false,
+        "tls": false,
+        "net": false,
+        "os" : false,
+        "constants" : false,
+        "vm" : false,
+        "querystring" : false,
+        "http": require.resolve("stream-http"),
+        "https": false,
+        "zlib": require.resolve("browserify-zlib") ,
+        "path": require.resolve("path-browserify"),
+        "stream": require.resolve("stream-browserify"),
+        "util": require.resolve("util/"),
+        "crypto": require.resolve("crypto-browserify")
+  }) 
+config.resolve.fallback = fallback; 
+config.plugins = (config.plugins || []).concat([ 
+   new webpack.ProvidePlugin({ 
+    process: 'process/browser', 
+  Buffer: ['buffer', 'Buffer'] 
+}) 
+]) 
     return config;
 };
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,10 +2,12 @@ import {Paper, Button} from '@mui/material/';
 
 import listes from './../../data/listes.json'
 
+
 export default function Header() {
+
     return (
         <Paper elevation={10}> 
-            {listes.map((liste) => (<Button color="success">{liste.name}</Button>))}
+            {listes.map((liste) => (<Button color="success"><img src={liste.icon} alt = "Logo de la liste" width="40" height="40" style={{margin: 5}}/>{liste.name}</Button>))}
         </Paper>
     )
 }
