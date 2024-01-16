8f201007552e674d49bb51ad4f3ff754b5012e07
nathan-claeys <127297857+nathan-claeys@users.noreply.github.com>
2024-01-16 10:45:00 +0100

    print logo

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,4 +1,4 @@
-import {Paper, Button} from '@mui/material/';
+import {Paper, Button, Stack} from '@mui/material/';
 
 import listes from './../../data/listes.json'
 import { Outlet, Link } from "react-router-dom";
@@ -9,7 +9,10 @@ export default function Header() {
     return (
         <>
         <Paper elevation={10}> 
+            <Stack direction = "row">
+                <img src={"assets/images/Logo allotomatique.png"} alt = "Logo allotomatique" width="80" height="80" style={{margin: 5}}/>
             {listes.map((liste) => (<Link to={"/liste"+liste.id}><Button color="success"><img src={liste.icon} alt = "Logo de la liste" width="40" height="40" style={{margin: 5}}/>{liste.name}</Button></Link>))}
+            </Stack>
         </Paper>
         <Outlet />
         </>
