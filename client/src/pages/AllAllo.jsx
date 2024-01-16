36d5208e22b0a4c933807ca2a8637225c49d1488
nathan-claeys <127297857+nathan-claeys@users.noreply.github.com>
2024-01-16 11:58:32 +0100

    more agility added

--- client/src/pages/AllAllo.jsx
+++ client/src/pages/AllAllo.jsx
@@ -1,5 +1,6 @@
 import { Stack } from '@mui/material';
 import Allo from '../components/Allo.jsx';
+import listes from './../../data/listes.json'
 
 import allos from './../../data/allotest.json'
 
@@ -7,11 +8,6 @@ import allos from './../../data/allotest.json'
 export default function AllAllo() {
     return (
         <Stack spacing={2}>
-        {allos["BDE1"].map((allo) => (<Allo id={allo.id} value={allo}/>))}
-        {allos["BDE2"].map((allo) => (<Allo id={allo.id} value={allo}/>))}
-        {allos["BDA1"].map((allo) => (<Allo id={allo.id} value={allo}/>))}
-        {allos["BDA2"].map((allo) => (<Allo id={allo.id} value={allo}/>))}
-        {allos["BDS1"].map((allo) => (<Allo id={allo.id} value={allo}/>))}
-        {allos["BDS2"].map((allo) => (<Allo id={allo.id} value={allo}/>))}
+            {listes.map((liste) => (allos[liste.id].map((allo) => (<Allo id={allo.id} value={allo}/>))))}
         </Stack>)
 }
