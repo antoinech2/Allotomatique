55de986b6520d12802ff5eec4527c2a95608e1fd
nathan-claeys <127297857+nathan-claeys@users.noreply.github.com>
2024-01-16 11:47:09 +0100

    all allos buttons

--- client/src/pages/AllAllo.jsx
+++ client/src/pages/AllAllo.jsx
@@ -1,8 +1,17 @@
+import { Stack } from '@mui/material';
+import Allo from '../components/Allo.jsx';
 
+import allos from './../../data/allotest.json'
 
 
 export default function AllAllo() {
     return (
-        <block></block>
-    )
+        <Stack spacing={2}>
+        {allos["BDE1"].map((allo) => (<Allo id={allo.id} value={allo}/>))}
+        {allos["BDE2"].map((allo) => (<Allo id={allo.id} value={allo}/>))}
+        {allos["BDA1"].map((allo) => (<Allo id={allo.id} value={allo}/>))}
+        {allos["BDA2"].map((allo) => (<Allo id={allo.id} value={allo}/>))}
+        {allos["BDS1"].map((allo) => (<Allo id={allo.id} value={allo}/>))}
+        {allos["BDS2"].map((allo) => (<Allo id={allo.id} value={allo}/>))}
+        </Stack>)
 }
