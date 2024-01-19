12050bea3cc89fe85cbc3f0ae93c442414d19e5c
nathan-claeys <127297857+nathan-claeys@users.noreply.github.com>
2024-01-19 16:16:17 +0100

    loading screen

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import { Stack, Button } from "@mui/material";
 import Liste from './pages/Liste';
 import Layout from './pages/Layout';
 import AllAllo from './pages/AllAllo';
@@ -7,16 +9,49 @@ import listes from './../data/listes.json'
 
 
 function App() {
-  return (
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Layout />}>
-          {listes.map((liste) => (<Route path={"liste"+liste.id} element={<Liste listeId={liste.id} />} />))}
-          <Route path={"/allAllo"} element={<AllAllo/>}/>
-      </Route>
-    </Routes>
-    </BrowserRouter>
-  );
+
+    const [visible, setVisible] = useState(true);
+    const [imageElement, setImageElement] = useState(null);
+    const [showPage, setShowPage] = useState(false);
+
+    const handleClick = () => {
+        setVisible(false);
+        setShowPage(true);
+
+        if (imageElement) {
+            imageElement.parentElement.removeChild(imageElement);
+        }
+    };
+
+    return (
+        <Stack>
+          <Button
+              onClick={() => { handleClick(); }}
+              color="primary"
+              size="large"
+              style={{ visibility: visible ? 'visible' : 'hidden' }}
+            >
+              Accedez au site
+            </Button>
+
+          {visible && (
+            <img src="assets/images/loadingScreen.png" alt="Image" style={{ visibility: visible ? 'visible' : 'hidden' }} />
+          )}
+
+          {showPage && <Page />}
+        </Stack>
+    );
 }
+
+const Page = () => (
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Layout />}>
+                {listes.map((liste) => (<Route path={"liste"+liste.id} element={<Liste listeId={liste.id} />} />))}
+                <Route path={"/allAllo"} element={<AllAllo/>}/>
+            </Route>
+        </Routes>
+    </BrowserRouter>
+);
 
 export default App;
